5f9c2aca85a6355817f53e52f38685e7317b1bad
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-09-21 19:43:44 +0300

    Update Stats.js

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 function Stats() {
   return (
     <div className="container p-3">
@@ -8,25 +9,25 @@ function Stats() {
           <h2 className="fs-4">Customer-first always</h2>
           <p className="text-muted">
             That's why 1.3+ crore customers trust Xcomtrading with Rs. 3.5+ lakh
-            crores<br></br> worth for equity investments.
+            crores<br /> worth for equity investments.
           </p>
           <h2 className="fs-4">No spam or gimmicks</h2>
           <p className="text-muted">
             No gimmicks, spam, "gamification", or annoying push notifications.
-            <br></br>
+            <br />
             High quality apps that you use at your pace, the way you like.
           </p>
           <h2 className="fs-4">The Zerodha universe</h2>
           <p className="text-muted">
             Not just an app, but a whole ecosystem. Our investments in 30+
-            <br></br>
+            <br />
             fintech startups offer you tailored services specific to your needs.
           </p>
 
           <h2 className="fs-4">Do better with money</h2>
           <p className="text-muted">
             With initiatives like Nudge and Kill Switch, we don't just
-            facilitate <br></br>transactions, but actively help you do better
+            facilitate <br />transactions, but actively help you do better
             with your money.
           </p>
         </div>
@@ -38,10 +39,10 @@ function Stats() {
           />
           <div className="text-center">
             <a href="/pricing" className="mx-5 text-decoration-none">
-              Explore our products  <i class="fa-solid fa-arrow-right-long"></i>
+              Explore our products <i className="fa-solid fa-arrow-right-long"></i>
             </a>
             <a href="/products" className="text-decoration-none">
-              Try Kite demo  <i class="fa-solid fa-arrow-right-long"></i>
+              Try Kite demo <i className="fa-solid fa-arrow-right-long"></i>
             </a>
           </div>
         </div>
@@ -51,3 +52,4 @@ function Stats() {
 }
 
 export default Stats;
+    
