08d6dd2b38056f946c00aa43718a714015f54072
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-09-21 16:59:09 +0300

    Update Hero.js

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
         <div className='container  p-5 mb-5 text-center border-bottom'>
             <h1 className='text-center fs-1 mb-3'>Technology</h1>
             <div className=' text-muted fs-5  mb-5 '><p>Sleek, modern and intuitive trading platforms</p>
-            <p>Check out our <a href='#' className='text-decoration-none'>investment offerings <i class="fa-solid fa-arrow-right-long"></i></a></p>
+            <p>Check out our <a href="/pricing"> className='text-decoration-none'>investment offerings <i class="fa-solid fa-arrow-right-long"></i></a></p>
             </div>
                 
             
