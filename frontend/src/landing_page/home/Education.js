8f6bbb0171b3cc5a6a0be44f3685e986c9acd967
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-09-21 19:47:48 +0300

    Update Education.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -1,29 +1,35 @@
 import React from 'react';
+
 function Education() {
     return ( 
         <div className='container'> 
-        <div className='row'>
-            <div className='col-6 mb-5 mt-5'>
-               <img src='media/images/education.svg'></img>
-            </div>
-            <div className='col-6 '>
-                <div className='mb-5 mt-5'>
-                    <h1 className='fs-2 mb-5'>Free and open market education</h1>
-                <p className="/education">Varsity. the largest online stock market education book in the world
-govering everything from the basics to advanced trading.</p>
-                <a href="" className="/support">Versity  <i class="fa-solid fa-arrow-right-long"></i></a>
+            <div className='row'>
+                <div className='col-6 mb-5 mt-5'>
+                    <img src='media/images/education.svg' alt='Education' />
                 </div>
-                    <p className="/education">TradingQ&A, the most active trading and investment community in
-India for all your market related queries.</p>
-                <a href="" className="text-decoration-none">TradingQ&A  <i class="fa-solid fa-arrow-right-long"></i></a>
-                <div>
-
+                <div className='col-6'>
+                    <div className='mb-5 mt-5'>
+                        <h1 className='fs-2 mb-5'>Free and open market education</h1>
+                        <p>
+                            Varsity, the largest online stock market education book in the world
+                            covering everything from the basics to advanced trading.
+                        </p>
+                        <a href="/education" className="text-decoration-none">
+                            Varsity <i className="fa-solid fa-arrow-right-long"></i>
+                        </a>
+                    </div>
+                    <p>
+                        TradingQ&A, the most active trading and investment community in
+                        India for all your market related queries.
+                    </p>
+                    <a href="/support" className="text-decoration-none">
+                        TradingQ&A <i className="fa-solid fa-arrow-right-long"></i>
+                    </a>
                 </div>
-                
             </div>
-        </div>
         </div>
      );
 }
 
 export default Education;
+                                
