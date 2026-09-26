85dfc84401d9cae23eaf4290153f30f12f2835a5
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-09-26 18:10:18 +0300

    Update OpenAccount.js

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -3,7 +3,7 @@ function OpenAccount() {
   return (
     <div className="container p-5 mb-5">
       <div className="row text-center">
-        <h1 className="mt-5 mb-3">Open a Zerodha account</h1>
+        <h1 className="mt-5 mb-3">Open a XcomTrading account</h1>
         <p className="mb-3 text-muted">
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
           F&O trades.
