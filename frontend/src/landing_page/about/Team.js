2bb5727ac0fcd6bf2fb3b4d58ca6775c6f3c5f5d
smartinvestsi254-prog <smartinvestsi254@gmail.com>
2026-09-26 18:14:26 +0300

    Update Team.js

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -17,7 +17,7 @@ function Team() {
                     </div>
                 </div>
                 <div className='col-6 text-muted mt-5' style={{ lineHeight: "1.8", fontSize: "1.2em" }}>
-                    <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+                    <p>Nithin bootstrapped and founded XcomTrading in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, XcomTrading has changed the landscape of the Indian broking industry.</p>
                     <p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
                     <p>Playing basketball is his zen</p>
                     <p>
